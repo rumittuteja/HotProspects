3d62bb3217473d04e04d746daf10ccb9fd2f7dc5
Rumit Singh Tuteja <rtuteja@i-360.com>
2024-06-11 16:24:52 -0400

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Rumit Singh Tuteja on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
